c05bd308cefb369d7973607afa03088323fdc8e1
vasilbekk <vasilbekk@mail.ru>
2021-03-22 15:48:22 +0300

    Добавлен init.py

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-from . import RedisStorage
+from redis_storage import RedisStorage
 
-def test()
+
+HOST = 'localhost'
+PORT = 6379
+DB = 1
+
+def test():
+    r = RedisStorage(host=HOST, port=PORT, db=DB)
+    print(r)
+
+
+if __name__ == '__main__':
+    test()
